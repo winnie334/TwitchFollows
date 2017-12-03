2eacbdcad87f0713f37e9f1b69b60350136b944d
winnie334 <winnie33winnie33@gmail.com>
2017-12-03 20:55:54 +0100

    Update TwitchFollow.py

--- TwitchFollow.py
+++ TwitchFollow.py
@@ -1,14 +1,3 @@
-from selenium import webdriver
-import pickle
+Progress has been discontinued in this public repository.
 
-
-if __name__ == '__main__':
-	url = 'https://www.twitchfollows.com/login'
-	driver = webdriver.Chrome(executable_path=r'C:\Users\winnie33\Downloads\chromedriver_win32\chromedriver.exe')
-	driver.get(url)
-
-
-"""
-<button onclick="AjaxLoader('Offer4618', 'func', 'new_follow', '&amp;offer=4618&amp;streamer=duckbit1');"
-class="btn btn-pink btn-lg">Follow &nbsp;(+25 <i class="fa fa-database" title="Credits"></i>)</button>
-"""
+If you want free money, go make your own programs! :^)
